b1ae3194d5b5cc00af5eb5960c5be1cebcf833f5
gunjan-lal <gunjan.vk@gmail.com>
2016-06-24 17:24:09 +0530

    Create properties.sh

---
+++ properties.sh
@@ -0,0 +1,8 @@
+export PROJECT=Final
+export BUCKET=LoadTesting_Final
+AMI="ami-fce3c696"
+KeyPairName="key_Gunjan"
+SecurityGroup="sg-ee751395"
+InstanceType="t2.micro"
+Subnet="subnet-a28a3dfa"
+NoOfInstances=1
