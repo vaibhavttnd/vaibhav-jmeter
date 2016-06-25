813a0ea52aac70c79eba02d5dc9b5129d39f99e3
gunjan-lal <gunjan.vk@gmail.com>
2016-06-25 21:09:12 +0530

    bn

---
+++ awscliconfig.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#install awscli
+sudo apt-get update
+sudo apt-get install awscli -y
+
+if [ `which aws` ]
+then
+echo "AWS CLI installed"
+else
+echo "AWS CLI not installed"
+fi
+
+#configure awscli
+echo -n "Enter Profile (leave empty for 'default'): "
+read Profile
+: ${Profile:=default}
+echo -n "Enter Access key: "
+read AK
+echo -n "Enter Secret Access Key: "
+read SAK
+echo -n "Enter region: "
+read Region
+echo -n "Enter output type (text/json): "
+read Output
+
+mkdir ~/.aws/ && touch ~/.aws/config
+
+cat <<here >> ~/.aws/config
+[${Profile}]
+region=$Region
+output=$Output
+here
+
+export AWS_ACCESS_KEY_ID=$AK
+export AWS_SECRET_ACCESS_KEY=$SAK
+export AWS_CONFIG_FILE="~/.aws/config"
+
+cat <<here1 >> user_data_file.sh
+cat <<here >> /var/lib/jenkins/.aws/config
+[default]
+region=$Region
+output=$Output
+here
+here1
+
+#cat <<here >> ./user_data_file.sh
+#export AWS_ACCESS_KEY_ID=$AK
+#export AWS_SECRET_ACCESS_KEY=$SAK
+#export AWS_DEFAULT_REGION=$Region
+#here
---
+++ user_data_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+touch /tmp/install.log
+> /tmp/install.log
+
+wget https://s3.amazonaws.com/$BUCKET/jenkins_install.sh -O /tmp/jenkins_install.sh
+wget https://s3.amazonaws.com/$BUCKET/jmeter_master.sh -O /tmp/jmeter_master.sh
+
+sudo bash /tmp/jmeter_master.sh >> /tmp/install.log
+sudo bash /tmp/jenkins_install.sh >> /tmp/install.log
+sudo apt-get install git -y
+sudo apt-get install awscli -y
+mkdir /var/lib/jenkins/.aws
+touch /var/lib/jenkins/.aws/config
+
+#cat <<here >> /var/lib/jenkins/.aws/config
+#[default]
+#region=us-east-1
+#output=json
+#here
