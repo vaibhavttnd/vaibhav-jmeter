347311d1bf72e37f31984cf2129b16b2d68d50bd
gunjan-lal <gunjan.vk@gmail.com>
2016-06-27 14:28:35 +0530

    name of slave

--- slave.sh
+++ slave.sh
@@ -9,6 +9,7 @@ while [ $count > 0 ]
 do
 InstanceID=$(aws ec2 run-instances --image-id $AMI --key-name $KeyPairName --security-group-ids $SecurityGroup --instance-type $InstanceType --subnet $Subnet --associate-public-ip-address --user-data file:///usr/share/jmeter/extras/configScriptSlave --output json | grep "InstanceId" | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
 sleep 20
+aws ec2 create-tags --resource $InstanceID --tags Key=Name,Value=Slave_$count_$PROJECT
 echo -ne `aws ec2 describe-instances --instance-id $InstanceID --output json | grep "PublicIpAddress" | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'` >> /usr/share/jmeter/extras/ip.txt
 echo -ne "," >> /usr/share/jmeter/extras/ip.txt
 sleep 20
