baffdcb4917a1544e9c5696a52776ac1dc96b1b9
gunjan-lal <gunjan.vk@gmail.com>
2016-07-08 15:55:22 +0530

    sudo ssh -t

--- launchJMeter.sh
+++ launchJMeter.sh
@@ -22,8 +22,8 @@ echo "Done!"
 
 ########### ssh into master
 echo "About to run tests!"
-SSH_COMMAND="bash -x /usr/share/jmeter/extras/jmeter_master.sh"
-sudo ssh -i JMeterKey.pem ubuntu@$MasterIP "${SSH_COMMAND}"
+#SSH_COMMAND="bash -x /usr/share/jmeter/extras/jmeter_master.sh"
+sudo ssh -i JMeterKey.pem ubuntu@$MasterIP -t "bash -x /usr/share/jmeter/extras/jmeter_master.sh"
 
 aws ec2 stop-instances --instance-ids $InstanceID
 ######display that tests are done
