e0040a5194fa105183e3161d852558d273b38559
pokekrishna <pokekrishna@gmail.com>
2016-07-26 11:00:59 +0530

    changing jmetertestprop for beta tothenew

--- JMetertestproperties.sh
+++ JMetertestproperties.sh
@@ -1,6 +1,6 @@
 export jmxFile=tothenew
 export OutputFile=tothenewresult
 export users=50,100,150,200
-export Load=200
+export Load=50
 export Threshold=01
-export JMeterKey=JMeterKey_tlwloh
+export JMeterKey=JMeterKey_qhirselnff
