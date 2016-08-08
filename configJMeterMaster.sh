0957b0ea1dccd5062923c729a7aaa8e20bee3f0c
pokekrishna <pokekrishna@gmail.com>
2016-08-08 17:56:30 +0530

    EC2instanceproperties.sh

--- configJMeterMaster.sh
+++ configJMeterMaster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PROJECT=;BUCKET_INSTALL=LoadTesting_;BUCKET_RESULT=LoadTestingResults_;
-Region=;Output=;
+PROJECT=loadtesting_wgryqvjxrr;BUCKET_INSTALL=LoadTesting_loadtesting_wgryqvjxrr;BUCKET_RESULT=LoadTestingResults_loadtesting_wgryqvjxrr;
+Region=us-east-1;Output=text;
 
 ############# PASSED AS USER DATA TO JMETER MASTER SERVER TO INSTALL JMETER & AWSCLI AND DOWNLOAD ALL FILES FROM S3
 
